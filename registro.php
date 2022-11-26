d7b405108a45d5e49cfb9f099cdce0288413e184
JGabriel0904 <jeysongabriel.jimenez@gmail.com>
2022-11-26 10:26:21 -0600

    Se eliminaron inyecciones SQL

--- registro.php
+++ registro.php
@@ -1,20 +1,13 @@
 <?php
-<<<<<<< HEAD
-  include ("conexion/Conexion.php");
-  //include ("Encryptar.php");
-  $bd = new Conexion();
-  //$enc = new Encryptar();
-=======
-  include ("models/conexion/Conexion.php");
-  //include ("Encryptar.php");
-  $bd = new Conexion();
-  //$enc = new Encryptar();
-  $mysqli = new mysqli("localhost", "root", "12345", "subastas");
->>>>>>> 88644aa51f56bc7d57bedbc32abb213a9a0d8337
-  session_start();
-  if(isset($_SESSION["id_usuario"])){
-    header("Location: index.php");
-  }
+include ("models/conexion/Conexion.php");
+//include ("Encryptar.php");
+$bd = new Conexion();
+//$enc = new Encryptar();
+$mysqli = new mysqli("localhost", "root", "12345", "subastas");
+session_start();
+if(isset($_SESSION["id_usuario"])){
+  header("Location: index.php");
+}
 ?>
 
 
@@ -23,141 +16,119 @@
 
 <head>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-    <title>Registrarme</title>
+  <title>Registrarme</title>
 
-    <!-- Favicon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon.png">
+  <!-- Favicon -->
+  <link rel="icon" type="image/png" sizes="16x16" href="img/favicon.png">
 
-    <!-- Bootstrap Core CSS -->
-<<<<<<< HEAD
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Bootstrap Core CSS -->
+  <link href="bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
-    <link href="css/sb-admin.css" rel="stylesheet">
+  <!-- Custom CSS -->
+  <link href="sb-admin.css" rel="stylesheet">
 
-    <!-- Morris Charts CSS -->
-    <link href="css/plugins/morris.css" rel="stylesheet">
-=======
-    <link href="bootstrap.min.css" rel="stylesheet">
+  <!-- Morris Charts CSS -->
+  <link href="plugins/morris.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
-    <link href="sb-admin.css" rel="stylesheet">
+  <!-- Custom Fonts -->
+  <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
-    <!-- Morris Charts CSS -->
-    <link href="plugins/morris.css" rel="stylesheet">
->>>>>>> 88644aa51f56bc7d57bedbc32abb213a9a0d8337
-
-    <!-- Custom Fonts -->
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
 
 </head>
 
 <body background="images/fondovaca.png">
 
-    <?php
-      if(isset($_POST["registro"])){
-        //echo "<script>alert('Entrar');</script>";
+  <?php
+    if(isset($_POST["registro"])){
+      //echo "<script>alert('Entrar');</script>";
 
-<<<<<<< HEAD
-        $correo = $_POST["correo"];
-        $user = $_POST["user"];
-        $pass = md5($_POST["pass"]);
+      //Datos que se piden para registrarse
+      $correo = $_POST["correo"];
+      $user = $_POST["user"];
+      $pass = $_POST["pass"];
 
-        $query = "INSERT into usuario(correo, user, pass) values('$correo','$user','$pass');";
+      //Sentencia SQL para agregar los datos insertados en el registro
+      $sentencia = $mysqli->prepare("INSERT into usuario(correo, user, pass) values('?','?','?');");
+      $sentencia->bind_param("sss", $correo,$user,$pass);
+      $sentencia->execute();
 
-        $result = $bd->query($query);
-
-        if($result == true){
-=======
-        //Datos que se piden para registrarse
-        $correo = $_POST["correo"];
-        $user = $_POST["user"];
-        $pass = $_POST["pass"];
-
-        //Sentencia SQL para agregar los datos insertados en el registro
-        $sentencia = $mysqli->prepare("INSERT into usuario(correo, user, pass) values('?','?','?');");
-        $sentencia->bind_param("sss", $correo,$user,$pass);
-        $sentencia->execute();
-
-        if($sentencia == true){
->>>>>>> 88644aa51f56bc7d57bedbc32abb213a9a0d8337
-          echo "<script>alert('Usuario registrado correctamente ve al login para que inicies sesion');</script>";
-          //header("Location: login.php");
-        }else{
-          echo "<script>alert('No se pudo registrar el usuario');</script>";
-        }
-
+      if($sentencia == true){
+        echo "<script>alert('Usuario registrado correctamente ve al login para que inicies sesion');</script>";
+        //header("Location: login.php");
+      }else{
+        echo "<script>alert('No se pudo registrar el usuario');</script>";
       }
-    ?>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-                <div class="login-panel panel panel-default">
-                    <div class="panel-heading">
-                      <div class="row">
-                        <div class="col-md-3">
-                          <img src="img/signup.png" width="65px">
-                        </div>
-                        <div class="col-md-9">
-                          <h3>Registrarme</h3>
-                        </div>
+    }
+  ?>
+
+  <div class="container">
+      <div class="row">
+          <div class="col-md-4 col-md-offset-4">
+              <div class="login-panel panel panel-default">
+                  <div class="panel-heading">
+                    <div class="row">
+                      <div class="col-md-3">
+                        <img src="img/signup.png" width="65px">
                       </div>
-
+                      <div class="col-md-9">
+                        <h3>Registrarme</h3>
+                      </div>
                     </div>
-                    <div class="panel-body">
-                        <form role="form" action="" method="post">
-                            <fieldset>
 
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Correo" name="correo" type="email" autofocus>
-                                </div>
+                  </div>
+                  <div class="panel-body">
+                      <form role="form" action="" method="post">
+                          <fieldset>
 
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Usuario" name="user" type="text">
-                                </div>
+                              <div class="form-group">
+                                  <input class="form-control" placeholder="Correo" name="correo" type="email" autofocus>
+                              </div>
 
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Contraseña" name="pass" type="password">
-                                </div>
+                              <div class="form-group">
+                                  <input class="form-control" placeholder="Usuario" name="user" type="text">
+                              </div>
 
-                                <!-- Change this to a button or input when using this as a form -->
-                                <input type="submit" name="registro" class="btn btn-success btn-block" style="background-color:#003817" value="Registrarme">
+                              <div class="form-group">
+                                  <input class="form-control" placeholder="Contraseña" name="pass" type="password">
+                              </div>
 
-                            </fieldset>
-                        </form>
-                    </div>
-                    <div class="panel-footer">
-                      <p>¿Ya estás registrado? <a href="login2.php">Inicia sesión aqui</a></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                              <!-- Change this to a button or input when using this as a form -->
+                              <input type="submit" name="registro" class="btn btn-success btn-block" style="background-color:#003817" value="Registrarme">
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+                          </fieldset>
+                      </form>
+                  </div>
+                  <div class="panel-footer">
+                    <p>¿Ya estás registrado? <a href="login2.php">Inicia sesión aqui</a></p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+  <!-- jQuery -->
+  <script src="js/jquery.js"></script>
 
-    <!-- Morris Charts JavaScript -->
-    <script src="js/plugins/morris/raphael.min.js"></script>
-    <script src="js/plugins/morris/morris.min.js"></script>
-    <script src="js/plugins/morris/morris-data.js"></script>
+  <!-- Bootstrap Core JavaScript -->
+  <script src="js/bootstrap.min.js"></script>
+
+  <!-- Morris Charts JavaScript -->
+  <script src="js/plugins/morris/raphael.min.js"></script>
+  <script src="js/plugins/morris/morris.min.js"></script>
+  <script src="js/plugins/morris/morris-data.js"></script>
 
 </body>
 
