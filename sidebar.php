96a88f768b713955b9bd435d87a3f0ae16314465
JGabriel0904 <jeysongabriel.jimenez@gmail.com>
2022-11-26 09:51:20 -0600

    Barra de navegación

---
+++ sidebar.php
@@ -0,0 +1,38 @@
+<<<<<<< HEAD
+<div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav side-nav">
+        <li>
+            <a href="index.php"><i class="fa fa-fw fa-dashboard"></i>Dashboard</a>
+        </li>
+        <li>
+            <a href="carrito.php"><i class="fa fa-fw fa-shopping-cart"></i> Carrito</a>
+        </li>
+        <li>
+            <a href="mis_subastas.php"><i class="fa fa-fw fa-th-list"></i> Mis subastas</a>
+        </li>
+        <li>
+            <a href="nueva_subasta.php"><i class="fa fa-fw fa-plus"></i> Nueva subasta</a>
+        </li>
+        <li>
+            <a href="nueva_raza.php"><i class="fa fa-fw fa-tags"></i> Nueva raza</a>
+=======
+<div class="collapse navbar-collapse navbar-ex1-collapse" style="background-color:#5B1F00">
+    <ul class="nav navbar-nav side-nav" style="background-color:#5B1F00">
+        <li>
+            <a href="index.php"><i class="fa fa-fw fa-dashboard" ></i><font color="white">Dashboard</font></a>
+        </li>
+        <li>
+            <a href="carrito.php"><i class="fa fa-fw fa-shopping-cart"></i> <font color="white">Carrito</font></a>
+        </li>
+        <li>
+            <a href="mis_subastas.php"><i class="fa fa-fw fa-th-list"></i> <font color="white">Mis subastas</font></a>
+        </li>
+        <li>
+            <a href="nueva_subasta.php"><i class="fa fa-fw fa-plus"></i> <font color="white">Nueva subasta</font></a>
+        </li>
+        <li>
+            <a href="nueva_raza.php"><i class="fa fa-fw fa-tags"></i> <font color="white">Nueva raza</font></a>
+>>>>>>> 88644aa51f56bc7d57bedbc32abb213a9a0d8337
+        </li>
+    </ul>
+</div>
