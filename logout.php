46f508d384f38e56de2406ebb1e2a679481e0635
JGabriel0904 <jeysongabriel.jimenez@gmail.com>
2022-11-26 09:44:24 -0600

    Cerrar sesión

---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php
+    session_start();
+    session_destroy();
+<<<<<<< HEAD
+    header("Location: login.php");
+=======
+    header("Location: login.php");//Cuando se cierrra sesión se redirige al login
+>>>>>>> 88644aa51f56bc7d57bedbc32abb213a9a0d8337
+?>
