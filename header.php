b65524aa0627fb97d3477204e1697296c1428352
JGabriel0904 <jeysongabriel.jimenez@gmail.com>
2022-11-26 09:36:49 -0600

    Cabecera de la app web

---
+++ header.php
@@ -0,0 +1,23 @@
+<ul class="nav navbar-right top-nav">
+    <li class="dropdown">
+<<<<<<< HEAD
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>  <b class="caret"></b></a>
+=======
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <b class="caret"></b></a>
+>>>>>>> 88644aa51f56bc7d57bedbc32abb213a9a0d8337
+        <ul class="dropdown-menu">
+            <li>
+                <a href="perfil.php"><i class="fa fa-fw fa-user"></i> Perfil</a>
+            </li>
+            <li class="divider"></li>
+            <li>
+                <a href="logout.php"><i class="fa fa-fw fa-power-off"></i> Cerrar sesion</a>
+            </li>
+        </ul>
+    </li>
+<<<<<<< HEAD
+</ul>
+
+=======
+</ul>
+>>>>>>> 88644aa51f56bc7d57bedbc32abb213a9a0d8337
