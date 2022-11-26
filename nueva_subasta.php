a3cfbf677256d387dd63bb4800db084f303145cb
JGabriel0904 <jeysongabriel.jimenez@gmail.com>
2022-11-26 10:21:57 -0600

    Eliminacion de inyecciones SQL

--- nueva_subasta.php
+++ nueva_subasta.php
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-
-=======
 <?php
 include("conexion/Conexion.php");
 //include ("Encryptar.php");
@@ -12,41 +9,12 @@ if (!isset($_SESSION["id_usuario"])) {
   header("Location: login.php");
 }
 ?>
->>>>>>> 88644aa51f56bc7d57bedbc32abb213a9a0d8337
 
 <!DOCTYPE html>
 <html lang="es">
 
 <head>
 
-<<<<<<< HEAD
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Nueva subasta</title>
-
-    <!-- Favicon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon.png">
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/sb-admin.css" rel="stylesheet">
-
-    <!-- Morris Charts CSS -->
-    <link href="css/plugins/morris.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-=======
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -73,157 +41,12 @@ if (!isset($_SESSION["id_usuario"])) {
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
->>>>>>> 88644aa51f56bc7d57bedbc32abb213a9a0d8337
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
 </head>
 
-<<<<<<< HEAD
-<body>
-
-
-
-    <div id="wrapper">
-
-        <!-- Navigation -->
-        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="subastas.php">Subastas</a>
-            </div>
-            <!-- Top Menu Items -->
-            <?php
-              include ("header.php");
-            ?>
-            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-            <?php
-              include ("sidebar.php");
-            ?>
-            <!-- /.navbar-collapse -->
-        </nav>
-
-        <div id="page-wrapper">
-
-            <div class="container-fluid">
-
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                            Subastas <small>Agregar nueva subasta</small>
-                        </h1>
-                        <ol class="breadcrumb">
-                            <li>
-                                <i class="fa fa-dashboard"></i> Dashboard
-                            </li>
-                            <li class="active">
-                                <i class="fa fa-plus"></i> Nueva subasta
-                            </li>
-                        </ol>
-                    </div>
-                </div>
-
-                      <div class="row">
-
-                        <form role="form" action="" method="post" enctype="multipart/form-data">
-
-                          <div class="col-lg-6">
-
-                                <h3>Detalle producto</h3>
-
-                                  <div class="form-group">
-                                      <label>Nombre</label>
-                                      <input type="text" name="nombre" class="form-control" required>
-                                  </div>
-
-                                  <div class="form-group">
-                                      <label>Descripcion</label>
-                                      <textarea name="descripcion" class="form-control" required></textarea>
-                                  </div>
-
-                                  <div class="form-group">
-                                      <label>Raza</label>
-                                      <select class="form-control" name="categoria">
-  
-                                      </select>
-                                      <p class="text-info">Si no esta la raza que busca puede agregar una desde el panel lateral.</p>
-                                  </div>
-
-                                  <div class="form-group">
-                                      <label>Foto</label>
-                                      <input type="file" name="foto">
-                                  </div>
-
-                        </div>
-                        <div class="col-lg-6">
-
-                              <h3>Detalle subasta</h3>
-
-                                  <div class="form-group">
-                                      <label>Precio minimo</label>
-                                      <input type="number" name="minimo" class="form-control">
-                                  </div>
-
-                                  <div class="form-group">
-                                      <label>Precio maximo</label>
-                                      <input type="number" name="maximo" class="form-control"  required>
-                                  </div>
-
-                                  <div class="form-group">
-                                      <label>Fecha de cierre</label>
-                                      <input type="date" name="fecha_fin" class="form-control" required>
-                                  </div>
-
-                                  <div class="form-group">
-                                      <label>Hora de cierre</label>
-                                      <input type="time" name="hora_fin" class="form-control" required>
-                                  </div>
-
-                                  <br>
-
-                                  <button name="agregar" type="submit" class="btn btn-success">Subastar</button>
-                                  <button type="reset" class="btn btn-danger">Cancelar</button>
-
-                          </div>
-
-                        </form>
-
-                      </div>
-                      <!-- /.row -->
-                  <br>
-
-            </div>
-            <!-- /.container-fluid -->
-
-        </div>
-        <!-- /#page-wrapper -->
-
-    </div>
-    <!-- /#wrapper -->
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-    <!-- Morris Charts JavaScript -->
-    <script src="js/plugins/morris/raphael.min.js"></script>
-    <script src="js/plugins/morris/morris.min.js"></script>
-    <script src="js/plugins/morris/morris-data.js"></script>
-
-</body>
-
-</html>
-=======
 <body style="background-color:#5B1F00">
 
   <?php
@@ -455,4 +278,3 @@ if (!isset($_SESSION["id_usuario"])) {
 </body>
 
 </html>
->>>>>>> 88644aa51f56bc7d57bedbc32abb213a9a0d8337
